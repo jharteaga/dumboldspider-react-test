7844e2e36e96a050fb4a1ee21a5fc0723cd0b49e
jharteaga <jh.arteaga09@gmail.com>
2020-08-12 22:46:47 -0700

    Add content in home component

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './home.component.scss';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div class="jumbotron">
+      <h1 class="display-4">DumbOldSpider LLC</h1>
+      <p class="lead">Exam for FrontEnd developers</p>
+      <Link class="btn btn-primary btn-lg" to="/about" role="button">
+        Who we are?
+      </Link>
     </div>
   );
 };
